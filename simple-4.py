78f6fe90c39b1e51d38ecc2fa24bd5a07ef2cd0f
Md. Abdulrauf <mdrauf.ac.mk11@gmail.com>
2025-06-29 13:43:13 +0530

    Update simple-4.py

--- simple-4.py
+++ simple-4.py
@@ -1,4 +1,4 @@
-total =0
+total = 0
 for num in range(1, 51):
-    total += num
-    print("The sum of numbers from 1 to 50 is:", total)
+    total += num  
+print("Total sum from 1 to 50 is:", total)
